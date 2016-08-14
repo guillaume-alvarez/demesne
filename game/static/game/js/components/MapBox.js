ee0ba5be18ec46a528d73e1ab716f99ea397084a
baaleze <baaleze@gmail.com>
2016-08-14 23:33:10 +0200

    Mise à jour du JS pour utiliser jquery (en plus ça marchait meme pas avant :p)

--- game/static/game/js/components/MapBox.js
+++ game/static/game/js/components/MapBox.js
@@ -1,39 +1,32 @@
 
-var map = document.getElementById('map');
+var map = $("#map");
 
 function mapChanged(event) {
-    var table = document.createElement('table');
+    var table = $('<table class ="table table-bordered" >');
     for (var x = 0; x < MAP_STORE.size(); x++){
-        var tr = document.createElement('tr');
+        var tr = $("<tr>");
         for (var y = 0; y < MAP_STORE.size(); y++){
-            var td = document.createElement('td');
+            var td = $('<td>');
             var node = MAP_STORE.node(x, y);
             var text = node ? node : 'empty';
-            td.appendChild(document.createTextNode(text));
-            tr.appendChild(td);
+            td.text(text);
+            tr.append(td);
         }
-        table.appendChild(tr);
+        table.append(tr);
     }
 
-    while (map.firstChild) {
-        map.removeChild(map.firstChild);
-    }
-    map.appendChild(table);
+    map.empty();
+    map.append(table);
 }
 
 function initButton() {
-    var button = document.createElement("button");
-    button.type = "button";
-    button.class = "btn btn-default btn-lg";
-    button.innerHTML = "Start game...";
-    button.onclick = function() { // Note this is a function
+    var button = $("<button>START GAME</button>").addClass("btn btn-success btn-lg").attr("type","button");
+    button.click(function() { // Note this is a function
         Actions.startGame(4);
-    };
+    });
 
-    while (map.firstChild) {
-        map.removeChild(map.firstChild);
-    }
-    map.appendChild(button);
+    map.empty();
+    map.append(button);
 }
 
 initButton();
