9f4937085c4d9e28e7f7c00f57d0dcd80e87abc9
galvarez <guillaume.alvarez@gmail.com>
2016-08-14 23:24:06 +0200

    Start work on data model.

--- game/models.py
+++ game/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Game(models.Model):
+    name = models.CharField(max_length=128, db_index=True, unique=True)
+    nb_players = models.IntegerField()
+    map_height = models.IntegerField()
+    map_width = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Player(models.Model):
+    name = models.CharField(max_length=128, db_index=True, unique=True)
+    game = models.ForeignKey(Game)
+
+    def __str__(self):
+        return self.name
+
+
+class Node(models.Model):
+    game = models.ForeignKey(Game)
+    x = models.IntegerField()
+    y = models.IntegerField()
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+
+class Place(models.Model):
+    node = models.ForeignKey(Node)
+    type = models.ForeignKey(Type)
