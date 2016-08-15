5c71f187f0233e91fe112c839c68f44b79aefaea
baaleze <baaleze@gmail.com>
2016-08-15 14:37:50 +0200

    Ajout des images + ajout du code qui affiche les informations de chaque case

--- game/static/game/js/components/MapBox.js
+++ game/static/game/js/components/MapBox.js
@@ -8,24 +8,39 @@ function mapChanged(event) {
         for (var y = 0; y < MAP_STORE.size(); y++){
             var td = $('<td>');
             var node = MAP_STORE.node(x, y);
-            var text = node ? node : '<div class="node">empty</div>';
+            var text = node ?
+                '<div class="node"><img src="'+window.staticUrl+'img/'+node.slug+'.jpg"></div>'
+                : '<div class="node">empty</div>';
             td.html(text);
             tr.append(td);
             // add popover
-            td.attr("title",node?node.title:'Empty tile');
+            td.attr("title",node?node.name:'Empty tile');
             td.popover({
                 toggle:"popover",
                 trigger:"hover",
                 container:"body",
-                content:"INFORMATIONS DETAILLEES (TODO)",
-                placement:"bottom"
+                content:formatInfo(node),
+                placement:"bottom",
+                html:true
             });
         }
         table.append(tr);
     }
 
-    $("#startgame").remove();
+    map.empty();
     map.append(table);
+}
+
+function formatInfo(node){
+    if(node){
+        var info = "<strong>"+(node.add_slot >0?"+"+node.add_slot+" SLOT<br>":"")
+        +(node.add_gold >0?"+"+node.add_gold+" <i class='fa fa-money'></i><br>":"")
+        +(node.add_buy >0?"+"+node.add_buy+" BUY<br>":"")
+        +"<em>"+node.description+"</em>"
+    }else{
+        var info = "<em>This tile is free and does nothing.</em>";
+    }
+    return info;
 }
 
 function initButton() {
@@ -33,6 +48,7 @@ function initButton() {
     button.click(function() { // Note this is a function
         Actions.startGame(4);
     });
+    map.empty();
     map.append(button);
 }
 
