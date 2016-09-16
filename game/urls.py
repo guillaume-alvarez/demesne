d7f4e66c4d09fe720f372e2c71e98a5b2fd1eaa7
baaleze <baaleze@gmail.com>
2016-09-16 22:22:19 +0200

    début authentication

--- game/urls.py
+++ game/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url, include
-
 from django.views.generic import RedirectView
-
-from . import views, rest
+from django.contrib.auth.views import login,logout
+from . import views, rest, form
 
 urlpatterns = [
     url(r'^$', RedirectView.as_view(url='games')),
+    url(r'^login/$', login, {'template_name':'game/login.html', 'authentication_form': form.LoginForm}),
+    url(r'^logout/$', logout, {'next_page': '/login'}),
+
     url(r'^games/(?P<game_id>[0-9]+)/$', views.load_game),
 
     url(r'^games/$', views.list_games),
--- game/views.py
+++ game/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.contrib.auth.decorators import login_required
 from .models import Game
 import logging
 
@@ -6,11 +7,13 @@ log = logging.getLogger(__name__)
 
 # Create your views here.
 
+@login_required(login_url="/login/")
 def load_game(request, game_id):
     game = get_object_or_404(Game, pk=game_id)
     context = { 'game_id': game.id, }
     return render(request, 'game/game.html', context)
 
+@login_required(login_url="/login/")
 def list_games(request):
     context = {'game_list':Game.objects.all()}
     return render(request,'game/games.html',context)
