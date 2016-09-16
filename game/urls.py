6eb8c537061f43930181b2ab20cfe937d57f5eee
baaleze <baaleze@gmail.com>
2016-09-16 23:12:18 +0200

    Il est maintenant possible de créer un compte et de se login / logout

--- game/urls.py
+++ game/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^$', RedirectView.as_view(url='games')),
     url(r'^login/$', login, {'template_name':'game/login.html', 'authentication_form': form.LoginForm}),
     url(r'^logout/$', logout, {'next_page': '/login'}),
+    url(r'^createUser/$', views.create_user),
 
     url(r'^games/(?P<game_id>[0-9]+)/$', views.load_game),
 
--- game/views.py
+++ game/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Game
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 import logging
 
 log = logging.getLogger(__name__)
 
 # Create your views here.
+
+def create_user(request):
+    login = request.GET["login"]
+    password = request.GET["password"]
+    user = User.objects.create_user(login,None,password)
+    user.save()
+    return HttpResponse(status=200)
 
 @login_required(login_url="/login/")
 def load_game(request, game_id):
