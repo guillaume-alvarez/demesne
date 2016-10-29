63b69167f0c0b984c5fe2943e2c9764bcb096383
galvarez <guillaume.alvarez@gmail.com>
2016-10-29 21:00:28 +0200

    Correct (again) static files path.

--- demesne/settings.py
+++ demesne/settings.py
@@ -136,7 +136,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, '../game'),
 ]
 
 # Simplified static file serving.
